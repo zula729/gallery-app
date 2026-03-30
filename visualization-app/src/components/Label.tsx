5d5afac3c4e699a90967104a2e44db055374e378
Azhar Massangaliyeva <550689@mail.muni.cz>
2026-03-30 19:35:26 +0200

    feat: add color for label type

--- visualization-app/src/components/Label.tsx
+++ visualization-app/src/components/Label.tsx
@@ -1,9 +1,19 @@
+type LabelProps = {
+    text: string;
+    type?: "keyword" | "tag" | "technology";
+}
 
-function Label({text} : {text: string}) {
+const colorMap = {
+    keyword:    "bg-blue-100 text-blue-700",
+    tag:        "bg-amber-100 text-amber-700",
+    technology: "bg-green-100 text-green-700",
+}
+
+function Label({ text, type = "keyword" }: LabelProps) {
     return (
-        <div className="rounded-xl bg-gray-200 flex items-center p-0.5 pl-2 pr-2">
+        <span className={`rounded-xl flex items-center p-0.5 pl-2 pr-2 ${colorMap[type]}`}>
             {text.charAt(0).toUpperCase() + text.slice(1).toLowerCase().replace(/_/g, " ")}
-        </div>
+        </span>
     );
 }
 
