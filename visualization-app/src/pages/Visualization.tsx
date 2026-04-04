78212308e4719d3e29ffa3cc12416a0816edde8e
Azhar Massangaliyeva <550689@mail.muni.cz>
2026-04-04 17:55:54 +0200

    feat: add recharts library

--- visualization-app/src/pages/Visualization.tsx
+++ visualization-app/src/pages/Visualization.tsx
@@ -1,4 +1,8 @@
 export function Visualization() {
-    return <> Visulization</>    
+    return (
+        <main className="flex-1 p-2 ml-4">
+            <h2 className="text-4xl font-semibold ">Visualization</h2>
+        </main>
+  );
 }
 export default Visualization;
---
+++ visualization-app/src/visualizations/RadarChart.tsx
@@ -0,0 +1,66 @@
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
+
+// #region Sample data
+const data = [
+  {
+    subject: 'Math',
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: 'Chinese',
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'English',
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'Geography',
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: 'Physics',
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: 'History',
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
+
+// #endregion
+const SimpleRadarChart = () => {
+  return (
+    <RadarChart
+      style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
+      responsive
+      outerRadius="80%"
+      data={data}
+      margin={{
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20,
+      }}
+    >
+      <PolarGrid />
+      <PolarAngleAxis dataKey="subject" />
+      <PolarRadiusAxis />
+      <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+    </RadarChart>
+  );
+};
+
+export default SimpleRadarChart;
