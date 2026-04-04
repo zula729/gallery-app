95fae96a8b5a08a7a1f1131a480582731c5b9124
Azhar Massangaliyeva <550689@mail.muni.cz>
2026-04-04 17:56:14 +0200

    feat: add home text

--- visualization-app/src/pages/Home.tsx
+++ visualization-app/src/pages/Home.tsx
@@ -1,8 +1,9 @@
 
 export function Home (){
     return (
-    <div > Home page
-    </div>
+        <main className="flex-1 p-2 ml-4">
+            <h2 className="text-4xl font-semibold ">Home</h2>
+        </main>
     );
 }
 export default Home;
