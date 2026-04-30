fa513768b5749709103c8f3b6efb06293a547a40
Azhar Massangaliyeva <550689@mail.muni.cz>
2026-04-30 12:48:07 +0200

    feat: add clickable text on top

--- visualization-app/src/components/Sidebar.tsx
+++ visualization-app/src/components/Sidebar.tsx
@@ -4,16 +4,20 @@ import { SidebarData } from './SidebarData';
 function Sidebar() {
     const navigate = useNavigate();
     const location = useLocation();
+
     return (
         <div className="sidebar h-screen w-52 pt-4 pr-3 pl-3 shadow-lg">
-            <p className="px-3 text-[20px] font-semibold uppercase tracking-widest text-gray-800 mb-2 pb-4">
+            <p
+                className="px-3 text-xl font-semibold uppercase tracking-widest text-gray-800 mb-2 pb-4 cursor-pointer"
+                onClick={() => navigate('/')}
+            >
                 PV251 Projects
             </p>
-            <p className="px-3 text-[11px] font-semibold uppercase tracking-widest text-gray-800 mb-2">
+            <p className="px-3 text-xs font-semibold uppercase tracking-widest text-gray-800 mb-2">
                 Navigation
             </p>
-            <hr className="border-gray-300 pb-2"></hr>
-            <ul className="">
+            <hr className="border-gray-300 pb-2" />
+            <ul>
                 {SidebarData.map((val, key) => {
                     const isActive =
                         val.link === '/'
@@ -23,16 +27,16 @@ function Sidebar() {
                         <li
                             key={key}
                             className={`group flex items-center gap-3 px-3 py-2.5 rounded-xl cursor-pointer mb-2
-                transition-all duration-150 ease-in-out
-              ${
-                  isActive
-                      ? 'bg-amber-50 text-amber-700'
-                      : 'text-gray-500 hover:bg-gray-50 hover:text-gray-900'
-              }`}
+                            transition-all duration-150 ease-in-out
+                            ${
+                                isActive
+                                    ? 'bg-amber-50 text-amber-700'
+                                    : 'text-gray-500 hover:bg-gray-50 hover:text-gray-900'
+                            }`}
                             onClick={() => navigate(val.link)}
                         >
-                            <div className="pr-2 ">{val.icon}</div>
-                            <div className=""> {val.title}</div>
+                            <div className="pr-2">{val.icon}</div>
+                            <div>{val.title}</div>
                         </li>
                     );
                 })}
