3737e663e6f631efcff221c6bc6e596d1bb6880d
Azhar Massangaliyeva <550689@mail.muni.cz>
2026-04-30 12:47:42 +0200

    fix: changed fontsize

--- visualization-app/src/components/Searchbar.tsx
+++ visualization-app/src/components/Searchbar.tsx
@@ -7,7 +7,7 @@ function Searchbar({ value, onChange }: SearchbarProps) {
     return (
         <input
             name="myInput"
-            className="rounded-lg border-solid border-2 border-gray-300 flex items-center p-1 w-full text-gray-500 "
+            className="rounded-lg border-solid border-2 border-gray-300 flex items-center p-1 w-full text-gray-500 font-normal"
             placeholder="Search for projects..."
             value={value}
             onChange={(e) => onChange(e.target.value)}
