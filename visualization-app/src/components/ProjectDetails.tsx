e2e0eb64262843acca6cb9909d614fb196a9d3ae
Azhar Massangaliyeva <550689@mail.muni.cz>
2026-05-04 22:04:47 +0200

    feat: add link to project details

--- visualization-app/src/components/ProjectDetails.tsx
+++ visualization-app/src/components/ProjectDetails.tsx
@@ -3,30 +3,38 @@ import { useCards } from '../hooks/useCards';
 import { useMemo, useState } from 'react';
 import Label from './Label';
 import ReactMarkdown from 'react-markdown';
+import Default from '../assets/default.png';
+import remarkGfm from 'remark-gfm';
 
 function ProjectDetails() {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const { id } = useParams<{ id: string }>();
     const cards = useCards();
     const card = useMemo(() => cards.find((c) => c.id === id), [cards, id]);
-
+    const getInitials = (name: string) =>
+        name
+            .split(' ')
+            .map((n) => n[0])
+            .join('')
+            .toUpperCase()
+            .slice(0, 2);
     if (!card) {
         return <div>No project</div>;
     }
 
     return (
         <main className="flex-1 p-8 ml-4">
-            <h2 className="text-4xl font-semibold mb-8">Gallery/{card.name}</h2>
+            <h2 className="text-4xl font-semibold mb-8">Gallery/{card.name ?? ''}</h2>
             <div className="w-full max-w-5xl">
-                <div className="relative w-full h-[600px] rounded-lg overflow-hidden mb-6 flex items-center justify-center">
+                <div className="relative w-full h-150 rounded-lg overflow-hidden mb-6 flex items-center justify-center">
                     <img
-                        src={card.images[currentImageIndex]}
+                        src={card.images?.[currentImageIndex] ?? Default}
                         aria-hidden
-                        className="absolute inset-0 w-full h-full object-cover blur-2xl scale-110 brightness-50"
+                        className="absolute inset-0 w-full h-full object-cover blur-2xl scale-110 brightness-100"
                     />
 
                     <img
-                        src={card.images[currentImageIndex]}
+                        src={card.images?.[currentImageIndex] ?? Default}
                         alt={`carousel-image-${currentImageIndex}`}
                         className="relative max-w-full max-h-full object-contain"
                     />
@@ -53,65 +61,83 @@ function ProjectDetails() {
             </div>
 
             <div className="max-w-5xl">
-                <div className="flex flex-col mb-6 ">
-                    <p className="font-semibold text-gray-700 mb-2">Category:</p>
-                    <div className="flex flex-nowrap gap-2 overflow-x-auto">
-                        {card.tags.map((tags) => (
-                            <Label key={tags} text={tags} type={'tag'}></Label>
-                        ))}
-                    </div>
-                    <p className="font-semibold text-gray-700 mb-2">Technology:</p>
-                    <div className="flex flex-wrap gap-2">
-                        {card.technology.map((technology) => (
-                            <Label key={technology} text={technology} type={'technology'}></Label>
-                        ))}
-                    </div>
-                    <p className="font-semibold text-gray-700 mb-2">Semester:</p>
-                    <div className="flex flex-wrap gap-2">
-                        <Label key={card.semestr} text={card.semestr} type={'semestr'}></Label>
-                    </div>
-                    <p className="font-semibold text-gray-700 mb-2">Keywords:</p>
-                    <div className="flex flex-wrap gap-2">
-                        {card.keywords.map((keyword) => (
-                            <Label key={keyword} text={keyword} type={'keyword'}></Label>
-                        ))}
-                    </div>
-                </div>
-                <h1 className="text-3xl font-bold mb-4">{card.name}</h1>
-                <p className="text-lg text-gray-700 mb-2">
-                    <strong>Author:</strong>{' '}
-                    {card.author.map((author, index) => (
-                        <span key={index}>
-                            {author}
-                            {index < card.author.length - 1 && ', '}
-                        </span>
+                <p className="font-semibold text-gray-700 mb-2">Category</p>
+                <div className="flex flex-wrap gap-2 ml-2 mb-6">
+                    {card.tags.map((tags) => (
+                        <Label key={tags} text={tags} type={'tag'}></Label>
                     ))}
-                </p>
-                <p className="text-lg text-gray-700">
-                    <strong>About:</strong>{' '}
-                    <div className="prose max-w-none mt-2">
-                        <ReactMarkdown
-                            components={{
-                                a: ({ href, children }) => (
-                                    <a
-                                        href={href}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="text-blue-500 underline"
-                                    >
-                                        {children}
-                                    </a>
-                                )
-                            }}
-                        >
-                            {card.text || 'No description provided'}
-                        </ReactMarkdown>
-                    </div>
-                    {/* {
-                        <ReactMarkdown>{card.text}</ReactMarkdown>
-                        // 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.'
-                    } */}
-                </p>
+                </div>
+                <p className="font-semibold text-gray-700 mb-2">Technologies</p>
+                <div className="flex flex-wrap gap-2 ml-2 mb-6">
+                    {card.technology.map((technology) => (
+                        <Label key={technology} text={technology} type={'technology'}></Label>
+                    ))}
+                </div>
+                <p className="font-semibold text-gray-700 mb-2">Semester</p>
+                <div className="flex flex-wrap gap-2 ml-2 mb-6">
+                    <Label key={card.semestr} text={card.semestr} type={'semestr'}></Label>
+                </div>
+                <p className="font-semibold text-gray-700 mb-2">Keywords</p>
+                <div className="flex flex-wrap gap-2 ml-2 mb-6">
+                    {card.keywords.map((keyword) => (
+                        <Label key={keyword} text={keyword} type={'keyword'}></Label>
+                    ))}
+                </div>
+                {card.link && (
+                    <>
+                        <p className="font-semibold text-gray-700 mb-2">Link</p>
+                        <div className="prose max-w-none mb-6 ml-2">
+                            <ReactMarkdown
+                                remarkPlugins={[remarkGfm]}
+                                components={{
+                                    a: ({ href, children }) => (
+                                        <a
+                                            href={href}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-amber-600 underline ml-2"
+                                        >
+                                            {children}
+                                        </a>
+                                    )
+                                }}
+                            >
+                                {card.link}
+                            </ReactMarkdown>
+                        </div>
+                    </>
+                )}
+                <p className="font-semibold text-gray-700 mb-2">Authors</p>
+                <div className="flex flex-row gap-4 mb-6 ml-2">
+                    {card.author.map((author, index) => (
+                        <div key={index} className="flex items-center gap-2">
+                            <div className="w-9 h-9 rounded-full border border-amber-500 bg-amber-50 flex items-center justify-center text-xs font-bold text-amber-800">
+                                {getInitials(author)}
+                            </div>
+                            <span className="text-gray-700  font-medium">{author}</span>
+                        </div>
+                    ))}
+                </div>
+                <p className="font-semibold text-gray-700 mb-2">About</p>
+                <div className="prose max-w-none mt-2 mb-6 ml-2">
+                    <ReactMarkdown
+                        remarkPlugins={[remarkGfm]}
+                        components={{
+                            a: ({ href, children }) => (
+                                <a
+                                    href={href}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-amber-600 underline"
+                                >
+                                    {children}
+                                </a>
+                            )
+                        }}
+                    >
+                        {card.text || 'No description provided'}
+                    </ReactMarkdown>
+                </div>
             </div>
         </main>
     );
