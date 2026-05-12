6d1230a061bcbd0af2d71fd3d839746e19ed9f10
zula729 <azhar.massa729@gmail.com>
2026-05-12 18:16:43 +0200

    fix: little fixes

--- visualization-app/vite.config.ts
+++ visualization-app/vite.config.ts
@@ -5,5 +5,6 @@ import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/PV251_Projects/',
     plugins: [react(), tailwindcss(), ViteYaml()]
 });
